00472be252e3d223c29461b54003a58f91962a09
Mimaamoo <156281534+Mimaamoo@users.noreply.github.com>
2024-08-15 16:40:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    // Dapatkan nilai dari form
-    var user_name = document.getElementById('name').value;
-    var user_message = document.getElementById('message').value;
-
-    // Buat object untuk dikirim ke emailJS
-    var emailParams = {
-        from_name: user_name,
-        to_name: 'Risol-In Aja',  // Nama penerima atau bisa dikosongkan
-        message: user_message
-    };
-
-    // Kirim email
-    emailjs.send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', emailParams)
+    console.log("Form submission intercepted. Sending form...");
+    
+    emailjs.sendForm('risoolinaja', 'risoolintemplate', this)
         .then(function(response) {
+            console.log("Email sent successfully:", response);
             document.getElementById('modal-body-content').innerText = 'Message sent successfully!';
             $('#responseModal').modal('show');
             document.getElementById('contact-form').reset();
         }, function(error) {
+            console.error('Failed to send email:', error);
             document.getElementById('modal-body-content').innerText = 'Message sending failed. Please try again.';
             $('#responseModal').modal('show');
-            console.log('Failed...', error);
         });
 });
